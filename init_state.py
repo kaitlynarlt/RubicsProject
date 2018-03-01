b467e35918d5bef1a03c9c693fbb5fb604314398
Peter Schultz <petersch@uw.edu>
2018-03-01 15:19:28 -0800

    fixed init state

--- init_state.py
+++ init_state.py
@@ -9,12 +9,13 @@ Y = 'Y'  # yellow
 O = 'O'  # orange
 W = 'W'  # white
 
-# this is a solvable initial state
-initial = {'F': [[RE, RE], [O, O]],  # front face
-           'U': [[W, Y], [W, Y]],  # "up" (top) face
-           'B': [[RE, RE], [O, O]],  # back face
+# this is a solvable initial state, to get there from
+# the goal state, use U2, R2, F2
+initial = {'F': [[O, O], [RE, RE]],  # front face
+           'U': [[Y, W], [Y, W]],  # "up" (top) face
+           'B': [[O, O], [RE, RE]],  # back face
            'L': [[BL, G], [G, BL]],  # left face
-           'D': [[Y, W], [Y, W]],  # "down" (bottom) face
+           'D': [[W, Y], [W, Y]],  # "down" (bottom) face
            'R': [[G, BL], [BL, G]]}  # right face
 
 # to solve: F2, R2, U2 <- check this
