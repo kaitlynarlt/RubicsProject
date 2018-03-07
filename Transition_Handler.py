1fdc29ee073fec1e74a04bf6dadef292d5b60e98
Kaitlyn Arlt <kaitlynarlt@gmail.com>
2018-03-07 04:25:47 +0000

    Update Transition_Handler.py

--- Transition_Handler.py
+++ Transition_Handler.py
@@ -15,6 +15,21 @@ GAMMA = 1.0 # discount (1.0 for no discount but we can change this later if we w
 
 POLICY = {}
 
+def handle_transition(action, new_state, reward):
+    
+    global PREVIOUS_STATE_FEATURES, Q_VALUES, ALPHA
+    
+    if not (PREVIOUS_STATE_FEATURES, action) in Q_VALUES:
+        Q_VALUES[(PREVIOUS_STATE_FEATURES, action)] = 0.0
+    
+    alpha_remainder = 1.0 - ALPHA
+    
+    Q_VALUES[(PREVIOUS_STATE_FEATURES, action)] = (Q_VALUES[(PREVIOUS_STATE_FEATURES, action)]*alpha_remainder) + (reward*ALPHA)
+    FEATURES_VISITED.append(PREVIOUS_STATE_FEATURES)
+    
+    PREVIOUS_STATE_FEATURES = new_state.features()
+    LAST_ACTION = action
+
 def extract_policy():
     
     global FEATURES_VISITED, ACTIONS, Q_VALUES, POLICY
