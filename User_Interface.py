096b13447ac4dbdfc2de63293ac1b1e1bf5e292a
Kaitlyn Arlt <kaitlynarlt@gmail.com>
2018-03-07 05:20:15 +0000

    Replace User_Interface.py: updated to support q learning

--- User_Interface.py
+++ User_Interface.py
@@ -1,7 +1,9 @@
 
 from Cube import State
 from Cube import OPERATORS
-
+from Transition_Handler import handle_transition
+from Transition_Handler import extract_policy
+#from Transition_Handler import HashableList
 
 STATE = State()
 
@@ -34,6 +36,16 @@ def take_turn():
     next_operator.apply(STATE)
     
     # push update all the stuff
+    reward = 0
+    if next_operator == OPERATORS[len(OPERATORS)-1] and STATE.is_goal_state():
+        reward = 100
+    handle_transition(next_operator, STATE, reward)
+
+    
 while RUNNING:
     take_turn()
-    
+
+policy = extract_policy()
+
+for op in policy:
+    print(op+" : "+str(policy[op]))
