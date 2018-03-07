e5d56f655a73e3e6e53e2dc326e94cab3e0d314b
Kaitlyn Arlt <kaitlynarlt@gmail.com>
2018-03-07 04:04:08 +0000

    Update Transition_Handler.py

--- Transition_Handler.py
+++ Transition_Handler.py
@@ -16,4 +16,19 @@ GAMMA = 1.0 # discount (1.0 for no discount but we can change this later if we w
 POLICY = {}
 
 def extract_policy():
+    
+    global FEATURES_VISITED, ACTIONS, Q_VALUES, POLICY
+    
+    for state in FEATURES_VISITED:
+        best_action = ACTIONS[0]
+        best_action_value = Q_VALUES[(state, best_action)]
+        for action in ACTIONS:
+            if Q_VALUES[(state, action)] > best_action_value:
+                best_action = action
+                best_action_value = Q_VALUES[(state, best_action)]
+        POLICY[state] = best_action
+    
+    POLICY[(State()).features()] = "Exit"
+    
+    return POLICY
 
